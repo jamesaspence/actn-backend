ba4d859ac98100ae81c778af047ceea091d2b942
James Spence <Jamesaspence20@gmail.com>
2020-04-02 14:49:50 -0500

    route to ensure auth is successful

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->group(function () {
-
+    Route::get('/auth', function () { return response(null); });
 });
 
 Route::post('/login', 'AuthController@login')->name('login');
