17c5b63a5179134b0e22b359f934985c78a71219
James Spence <Jamesaspence20@gmail.com>
2020-04-05 14:04:37 -0500

    added email to hidden attribute in user
    User will not expose any data short of username and id by default.

--- app/Models/User.php
+++ app/Models/User.php
@@ -35,7 +35,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token', 'created_at', 'updated_at'
+        'password', 'remember_token', 'created_at', 'updated_at', 'email'
     ];
 
     /**
