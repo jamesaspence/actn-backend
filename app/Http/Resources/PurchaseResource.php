2615d717447b225d6b1a5bddf5927b0367412e23
James Spence <Jamesaspence20@gmail.com>
2020-04-05 14:00:39 -0500

    new api resources for user, purchase

---
+++ app/Http/Resources/PurchaseResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\User;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @property User user
+ * @property integer quantity
+ * @property string date
+ * @property integer price
+ * @property integer id
+ */
+class PurchaseResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'price' => $this->price,
+            'date' => $this->date,
+            'quantity' => $this->quantity,
+            'user' => new PublicUser($this->user)
+        ];
+    }
+}
